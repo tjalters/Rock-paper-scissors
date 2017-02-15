4f3871848a6b986e0b7e291bdee32362565812bb
tjalters <tjalters32@gmail.com>
2017-02-15 12:25:21 -0600

    first upload

---
+++ rockpaperscissors/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rockpaperscissors
+{
+    public class Game
+    {
+        public string rock;
+        public string paper;
+        public string scissors;
+        public string lizard;
+        public string spock;
+        public string PlayerOne;
+        public string PlayerTwo;
+        public string PlayerAi;
+        public bool PlayerOneWins;
+        public bool PlayerTwoWins;
+        public bool PlayerAiWins;
+    }
+}
